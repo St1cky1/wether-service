050ba29c668f28c11e85e3d16e27dc463f9f03da
St1cky1 <VladPetrovArtem@mail.ru>
2025-10-29 21:42:35 +0300

    add const HTTP port

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+const portHttp = ":3000"
+
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
@@ -18,7 +20,7 @@ func main() {
 		}
 	})
 
-	if err := http.ListenAndServe(":3000", r); err != nil {
+	if err := http.ListenAndServe(portHttp, r); err != nil {
 		panic(err)
 	}
 
